ae6237bec87303818f1d7c3b2cff80b63304eec8
Eddie <jhih.ching@gmail.com>
2025-03-07 02:57:41 +0800

    Implement q_insert_head and q_insert_tail
    The q_insert_head function inserts an element at the head of the queue,
    while q_insert_tail appends an element at the tail.
    
    Change-Id: I570e3008174fa7f56915e3a2794c98af59b953a1

--- queue.c
+++ queue.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "list.h"
 
 #include "queue.h"
 
@@ -31,12 +32,24 @@ void q_free(struct list_head *head)
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+    element_t *ele = malloc(sizeof(element_t));
+    ele->value = strdup(s);
+    list_add(&ele->list, head);
+
     return true;
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+    element_t *ele = malloc(sizeof(element_t));
+    ele->value = strdup(s);
+    list_add_tail(&ele->list, head);
+
     return true;
 }
 
